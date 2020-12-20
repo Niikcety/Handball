4e31c7c697d8a33ccdde1a1b3356bfab49ba4ac4
Nikolay <nikolaineshev1999@gmail.com>
2020-12-20 23:46:03 +0200

    Changed the implementation of match_create

--- src/match/forms.py
+++ src/match/forms.py
@@ -5,6 +5,5 @@ from .models import Match
 
 class CreateForm(forms.Form):
     match_info = forms.CharField(
-        max_length=2000,
-        widget=forms.Textarea(),
+        widget=forms.Textarea()
     )
--- src/match/views.py
+++ src/match/views.py
@@ -8,6 +8,12 @@ from team.models import Team
 from .models import Match
 from .forms import CreateForm
 
+from queue import Queue
+import time
+import random
+import threading
+import concurrent.futures
+
 # Create your views here.
 def list_matches(request):
     matches = Match.objects.all()
@@ -17,40 +23,50 @@ def list_matches(request):
     return render(request, "match/matches_list.html", context)
 
 
-def save_matches(tokens):
-    if Team.objects.filter(name=tokens['home_team']).count() == 0:
-        Team(name=tokens['home_team'], wins=0).save()
-    if Team.objects.filter(name=tokens['away_team']).count() == 0:
-        Team(name=tokens['away_team'], wins=0).save()
+def tokenize_matches(queue, match_info):
+    for match in match_info:
+        if match == 'stop':
+            break
+        queue.put(Match.tokenizer(match.replace('\r', '')))
 
-    team1_id = Team.objects.filter(name=tokens['home_team']).values('id')[0]['id']
-    team2_id = Team.objects.filter(name=tokens['away_team']).values('id')[0]['id']
-    team1 = Team.objects.filter(name=tokens['home_team'])[0]
-    team2 = Team.objects.filter(name=tokens['away_team'])[0]
-    if Match.objects.filter(Q(home_team=team1_id) & Q(away_team=team2_id)).count() == 0:
-        Match(home_team=team1, away_team=team2, home_score=tokens['first_match_home'], away_score=tokens['first_match_away']).save()
-    if Match.objects.filter(home_team=team2).filter(away_team=team1).count() == 0:
-        Match(home_team=team2, away_team=team1, home_score=tokens['second_match_home'], away_score=tokens['second_match_away']).save()
-    
-    winner_name = Match.get_winner(tokens)
-    winner_team = Team.objects.get(name=winner_name)
-    winner_team.wins += 1
-    winner_team.save()
+
+def save_matches(queue):
+    time.sleep(random.random())
+    while not queue.empty():
+        tokens = queue.get()
+        if Team.objects.filter(name=tokens['home_team']).count() == 0:
+            Team(name=tokens['home_team'], wins=0).save()
+        if Team.objects.filter(name=tokens['away_team']).count() == 0:
+            Team(name=tokens['away_team'], wins=0).save()
+
+        team1_id = Team.objects.filter(name=tokens['home_team']).values('id')[0]['id']
+        team2_id = Team.objects.filter(name=tokens['away_team']).values('id')[0]['id']
+        team1 = Team.objects.filter(name=tokens['home_team'])[0]
+        team2 = Team.objects.filter(name=tokens['away_team'])[0]
+        if Match.objects.filter(Q(home_team=team1_id) & Q(away_team=team2_id)).count() == 0:
+            Match(home_team=team1, away_team=team2, home_score=tokens['first_match_home'], away_score=tokens['first_match_away']).save()
+        if Match.objects.filter(home_team=team2).filter(away_team=team1).count() == 0:
+            Match(home_team=team2, away_team=team1, home_score=tokens['second_match_home'], away_score=tokens['second_match_away']).save()
+        
+        winner_name = Match.get_winner(tokens)
+        winner_team = Team.objects.get(name=winner_name)
+        winner_team.wins += 1
+        winner_team.save()
+
 
 def match_create(request):
     if request.method == 'POST':
         form = CreateForm(request.POST)
-        match_info = request.POST.get('match_info')
-        if match_info == 'stop':
-            return render(request, 'match/index.html')
-        if Match.is_valid(match_info):
-            tokens = Match.tokenizer(match_info)
-            save_matches(tokens)
-            messages.info(request, 'Match added successfully!')
-            return render(request, 'match/match_create.html', {'form': CreateForm()})
-        else:
-            messages.info(request, 'Wrong input data. Try again.')
-            return render(request, 'match/match_create.html', {'form': CreateForm()})
+        match_info = request.POST.get('match_info').split('\n')
+        queue = Queue(maxsize=1000)
+        thread_read = threading.Thread(target=tokenize_matches, args=[queue, match_info])
+        thread_write = threading.Thread(target=save_matches, args=[queue])
+        thread_read.start()
+        thread_write.start()
+        thread_read.join()
+        thread_write.join()
+        messages.success(request, 'Success!')
+        return render(request, 'match/match_create.html', {'form': CreateForm()})
     else:
         form = CreateForm()
     context = {
